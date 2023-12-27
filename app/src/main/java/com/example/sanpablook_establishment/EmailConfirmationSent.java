4982eca074b59af39fb7e490b463fc0b3b7a822a
Trisha Aguilera <aguileratrisha31@gmail.com>
2023-12-27 21:28:40 +0800

    Committing changes before merge

---
+++ app/src/main/java/com/example/sanpablook_establishment/EmailConfirmationSent.java
@@ -0,0 +1,14 @@
+package com.example.sanpablook_establishment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmailConfirmationSent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_confirmation_sent);
+    }
+}
