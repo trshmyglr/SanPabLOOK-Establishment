e5eef104ae64b0e8a373b8a6f086b6459dc62c9c
Leona Dayang <leyowwnadayang@gmail.com>
2023-12-27 21:34:35 +0800

    added settings page and dialog boxes

--- app/src/main/java/com/example/sanpablook_establishment/ProfileFragment.java
+++ app/src/main/java/com/example/sanpablook_establishment/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.sanpablook_establishment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +8,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 public class ProfileFragment extends Fragment {
+
+    ImageButton btnSettings;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,7 +23,23 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        btnSettings = view.findViewById(R.id.settingsButton);
+
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToSettings(view);
+            }
+        });
+
+        return view;
+    }
+
+    private void goToSettings(View view) {
+        Intent intent = new Intent(getActivity(), ProfileSettingsActivity.class);
+        startActivity(intent);
     }
 }
